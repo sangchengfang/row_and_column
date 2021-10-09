cc49a90b072cdf9d957ee1e0f603a9472866d62d
sangchengfang <1425853773@qq.com>
2021-10-09 21:50:51 +0800

    Add main() function so this script can be used as a module and imported in other python script

--- row2column.py
+++ row2column.py
@@ -47,25 +47,31 @@ def row2column(input_file, output_file):
 
 # main program
 # Read command line arguments
-if len(sys.argv) == 1:
-    print('Correct usage: row2column.py input_file_name output_file_name')
-    sys.exit()
-else:
-    Ginput_file = sys.argv[1]
-    Goutput_file = sys.argv[2]
+# main()
+def main():
+    if len(sys.argv) == 1:
+        print('Correct usage: row2column.py input_file_name output_file_name')
+        sys.exit()
+    else:
+        Ginput_file = sys.argv[1]
+        Goutput_file = sys.argv[2]
 
-# check if the file exists
-if not os.path.isfile(Ginput_file):
-    print("Cannot find input file in your system")
-    sys.exit()
+    # check if the file exists
+    if not os.path.isfile(Ginput_file):
+        print("Cannot find input file in your system")
+        sys.exit()
 
-file_path = os.path.split(Ginput_file)
-print(file_path)
+    file_path = os.path.split(Ginput_file)
+    print(file_path)
 
-if not os.path.exists(file_path[0] + '/' + Goutput_file):
-    print("Cannot find output file in your system, now create it")
-    cmd = 'touch ' + file_path[0] + '/' + Goutput_file
-    print(cmd)
-    os.system(cmd)
+    if not os.path.exists(file_path[0] + '/' + Goutput_file):
+        print("Cannot find output file in your system, now create it")
+        cmd = 'touch ' + file_path[0] + '/' + Goutput_file
+        print(cmd)
+        os.system(cmd)
 
-row2column(Ginput_file, file_path[0] + '/' + Goutput_file)
+    row2column(Ginput_file, file_path[0] + '/' + Goutput_file)
+
+
+if __name__ == "__main__":
+    main()
